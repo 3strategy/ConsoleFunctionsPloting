5d3fda84bc969002f892193fcb0c8f85f2637600
Guy Siedes <3strategy@gmail.com>
2023-11-20 12:06:00 +0200

    Switched to a graphical solution.

--- Program.cs
+++ Program.cs
@@ -1,33 +1,63 @@
 ﻿using System;
+using Unit4.TurtleLib;
 
 namespace ConsoleFunctionsPloting
 {
     class Program
     {
+        static double currentAngle = 90; //the turtle starts facing north
+        static double x1 = 0, y1 = 0;
+
         static void Main(string[] args)
         {
-            for (int x = 0; x < 7; x++)
-            {
-                Console.SetCursorPosition(x, (int)f(x));
-                Console.Write("*");
-                if (x > 2) Console.Write($"({x},{f(x)})");
+            Turtle t = new Turtle();
+            t.TailDown();
+            t.SetVisible(false);
+            DrawAxes(t);
+            DrawLine(t, -5, f(-5));
+            t.TailDown();
+            for (int x = 1 + (int)x1; x <= 5; x++)
+                DrawLine(t, x, f(x));
 
-                Console.SetCursorPosition(x, (int)g(x));
-                Console.Write(".");
-                if (x > 2) Console.Write($"({x},{g(x)})");
-            }
-            Console.ReadLine();
+        }
+        /// <summary>
+        /// y=x^2
+        /// </summary>
+        static double f(double x) => x * x;
+
+        static void DrawAxes(Turtle tuti)
+        {
+            DrawLine(tuti, 0, 10);
+            DrawLine(tuti, 0, -10);
+            tuti.TailUp();
+            DrawLine(tuti, -10, 0);
+            tuti.TailDown();
+            DrawLine(tuti, 10, 0);
+            tuti.TailUp();
+        }
+        static void DrawLine(Turtle tur, double x2, double y2)
+        {
+            double angle = CalculateAngle(x1, y1, x2, y2);
+            double distance = DistanceBetweenPoints(x1, y1, x2, y2);
+            RotateTurtle(tur, angle);
+            tur.MoveForward(distance);
+            currentAngle = angle; // Update the current angle
+            x1 = x2; y1 = y2; // Update the current point into x1,y1.
         }
 
-        // באופן חריג רק היום מרשה לפונקציה להתחיל באות קטנה
-        static double g(double x)
+        static double CalculateAngle(double x1, double y1, double x2, double y2)
         {
-            double k = 3;
-            return k * f(x - 1) - 3;
+            return Math.Atan2(y2 - y1, x2 - x1) * (180 / Math.PI); //calculate angle from slope.
         }
-        static double f(double x)
+
+        static double DistanceBetweenPoints(double x1, double y1, double x2, double y2)
         {
-            return ((x - 3) * (x - 3) + 3);
+            return 10 * Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2)); //10* for scale
+        }
+
+        static void RotateTurtle(Turtle tutu, double targetAngle)
+        {
+            tutu.TurnRight(currentAngle - targetAngle);
         }
     }
 
